1003f383f4ab9a2bf5e2ea1145f36049b3ea5311
sumankaundinya <sumankaundinya@gmail.com>
2025-02-07 15:39:57 +0100

    Update startupNameGenerator.js

--- startupNameGenerator.js
+++ startupNameGenerator.js
@@ -5,8 +5,9 @@ const secondWords = ["Solutions", "Technologies", "Ventures", "Systems", "Enterp
 const randomNumber1 = Math.floor(Math.random() * 10); // First word index
 const randomNumber2 = Math.floor(Math.random() * 10); // Second word index
 
-const startupName = firstWords[randomNumber1] + " " + secondWords[randomNumber2];
+const startupName = `${firstWords[randomNumber1]} ${secondWords[randomNumber2]}`;
 
 // To find the number of characters in the startup name
 const nameLength = startupName.length;
-console.log("The startup: \"" + startupName + "\" contains " + nameLength + " characters.");
+
+console.log(`The startup: "${startupName}" contains ${nameLength} characters.`);
