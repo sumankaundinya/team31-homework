ab2288516b9e79288d30ce592dbbf05108d1ac8f
sumankaundinya <sumankaundinya@gmail.com>
2025-02-07 15:36:38 +0100

    Update housePriceCalculator.js

--- housePriceCalculator.js
+++ housePriceCalculator.js
@@ -3,33 +3,33 @@ const peterHouseWidth = 8;
 const peterHouseDepth = 10;
 const peterHouseHeight = 10;
 const peterHouseGardenSize = 100;
-const peterHouseCost = 2500000;
+const peterActualHouseCost = 2500000;
 
 // Julia's house details
-const juliaWidth = 5;
-const juliaDepth = 11;
-const juliaHeight = 8;
-const juliaGardenSize = 70;
-const juliaHouseCost = 1000000;
+const juliaHouseWidth = 5;
+const juliaHouseDepth = 11;
+const juliaHouseHeight = 8;
+const juliaHouseGardenSize = 70;
+const juliaActualHouseCost = 1000000;
 
 // House price formula: volume * 2.5 * 1000 + garden size * 300
-const peterHousePrice = (peterWidth * peterDepth * peterHeight * 2.5 * 1000) + (peterGardenSize * 300);
-const juliaHousePrice = (juliaWidth * juliaDepth * juliaHeight * 2.5 * 1000) + (juliaGardenSize * 300);
+const peterEstimatedHousePrice = (peterHouseWidth * peterHouseDepth * peterHouseHeight * 2.5 * 1000) + (peterHouseGardenSize * 300);
+const juliaEstimatedHousePrice = (juliaHouseWidth * juliaHouseDepth * juliaHouseHeight * 2.5 * 1000) + (juliaHouseGardenSize * 300);
 
 // Checking if Peter is paying too much or too little
-if (peterHousePrice > peterHouseCost) {
-    console.log("Peter is paying too little. The house is worth " + peterHousePrice + ".");
-} else if (peterHousePrice < peterHouseCost) {
-    console.log("Peter is paying too much. The house is worth " + peterHousePrice + ".");
+if (peterEstimatedHousePrice > peterActualHouseCost) {
+    console.log(`Peter is paying too little. The house is worth ${peterEstimatedHousePrice}.`);
+} else if (peterEstimatedHousePrice < peterActualHouseCost) {
+    console.log(`Peter is paying too much. The house is worth ${peterEstimatedHousePrice}.`);
 } else {
     console.log("Peter is paying the right price.");
 }
 
 // Checking if Julia is paying too much or too little
-if (juliaHousePrice > juliaHouseCost) {
-    console.log("Julia is paying too little. The house is worth " + juliaHousePrice + ".");
-} else if (juliaHousePrice < juliaHouseCost) {
-    console.log("Julia is paying too much. The house is worth " + juliaHousePrice + ".");
+if (juliaEstimatedHousePrice > juliaActualHouseCost) {
+    console.log(`Julia is paying too little. The house is worth ${juliaEstimatedHousePrice}.`);
+} else if (juliaEstimatedHousePrice < juliaActualHouseCost) {
+    console.log(`Julia is paying too much. The house is worth ${juliaEstimatedHousePrice}.`);
 } else {
     console.log("Julia is paying the right price.");
 }
