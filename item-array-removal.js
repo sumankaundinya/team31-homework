db7bfcb61ed77ce1594afa311e47ed0b2051f115
sumankaundinya <sumankaundinya@gmail.com>
2025-02-20 20:35:17 +0100

    Added JavaScript Week 3 homework

---
+++ item-array-removal.js
@@ -0,0 +1,20 @@
+function removeName(namesArray, nameToRemove) {
+  return namesArray.filter((name) => name !== nameToRemove);
+}
+
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+];
+const nameToRemove = "Ahmad";
+
+const updatedNames = removeName(names, nameToRemove);
+
+console.log(updatedNames);
+// Output: ["Peter", "Yana", "kristina", "Rasmus", "Samuel", "katrine", "Tala"]
---
+++ travel-information.js
@@ -0,0 +1,17 @@
+console.log("======== travel-information  ========");
+
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+function getTravelTime(information) {
+  const travelTimePerHour = information.destinationDistance / information.speed;
+  const hours = Math.floor(travelTimePerHour);
+  const minutes = (travelTimePerHour - hours) * 60;
+
+  return `${hours} hours and ${Math.floor(minutes)} minutes`;
+}
+
+const travelTime = getTravelTime(travelInformation);
+console.log(travelTime); // 8 hours and 38 minutes
